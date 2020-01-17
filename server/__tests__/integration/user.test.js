1eaeb0e4e324a7cb35bb92ea7b00d50bc5cd1c43
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-01-16 21:21:10 -0300

    password encryption

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,19 +1,31 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
-
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Nathalia Cristina Gomes Oliveira',
+      email: 'nathaliacristina20@hotmail.com',
+      password: '123456',
+    });
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Nathalia Cristina Gomes Oliveira',
         email: 'nathaliacristina20@hotmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
@@ -25,7 +37,7 @@ describe('User', () => {
       .send({
         name: 'Nathalia Cristina Gomes Oliveira',
         email: 'nathaliacristina20@hotmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     const response = await request(app)
@@ -33,7 +45,7 @@ describe('User', () => {
       .send({
         name: 'Nathalia Cristina Gomes Oliveira',
         email: 'nathaliacristina20@hotmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.status).toBe(400);
